cb350042d880f65c1c331d7b2d971fa993ade8ac
Fanny Vazart <fanny.vazart@le-campus-numerique.fr>
2023-09-05 10:17:49 +0200

    3.3 Catalogue avec tableau multidimensionnel + 3.2 corrigé

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -47,61 +47,61 @@ $Ecran = [
 
         <section class="souris">
             <section class="nom_prod">
-                <h2>Souris</h2>
+                <h2><?php echo $Souris['name'] ?></h2>
             </section>  
             <section class="description_prod">
-                Cette magnifique souris est, en plus, très ergonomique.
+                <?php echo $Souris["description"] ?>
             </section>
             <section>
-                <img class="photo_prod" src="https://www.grosbill.com/images_produits/fbab70f8-5842-4c66-9a8f-ac6d92543d64.jpg" alt="photo du produit">
+                <img class="photo_prod" src="<?php echo $Souris["photo"] ?>" alt="photo du produit">
             </section>
             <section class="prix_prod">
-                Prix : 8 €
+                Prix : <?php echo $Souris["price"] ?> €
             </section>
         </section>
 
         <section class="tour">
             <section class="nom_prod">
-                <h2>Tour</h2>
+                <h2><?php echo $Tour["name"] ?></h2>
             </section>  
             <section class="description_prod">
-            Cette magnifique tour est, en plus, très fonctionnelle.
+                <?php echo $Tour["description"] ?>
             </section>
             <section>
-                <img class="photo_prod" src="https://www.ordi2-0.fr/wp-content/uploads/2021/02/Meilleur-ordinateur-tour-Sedatech-253x300.jpg" alt="photo du produit">
+                <img class="photo_prod" src="<?php echo $Tour["photo"] ?>" alt="photo du produit">
             </section>
             <section class="prix_prod">
-                Prix : 560 €
+                Prix : <?php echo $Tour["price"] ?> €
             </section>
         </section>
 
         <section class="clavier">
             <section class="nom_prod">
-                <h2>Clavier</h2>
+                <h2><?php echo $Clavier["name"] ?></h2>
             </section>  
             <section class="description_prod">
-            Ce magnifique clavier est, en plus, très pratique.
+                <?php echo $Clavier["description"] ?>
             </section>
             <section>
-                <img class="photo_prod" src="https://i0.wp.com/sesamepc.com/wp-content/uploads/2021/05/Clavier-US-HP-840-G1-G2-850-G1-G2-dessus.jpg?fit=1200%2C800&ssl=1" alt="photo du produit">
+                <img class="photo_prod" src="<?php echo $Clavier["photo"] ?>" alt="photo du produit">
             </section>
             <section class="prix_prod">
-                Prix : 270 €
+                Prix : <?php echo $Clavier["price"] ?> €
             </section>
         </section>
 
         <section class="ecran">
             <section class="nom_prod">
-                <h2>Ecran</h2>
+                <h2><?php echo $Ecran["name"] ?></h2>
             </section>  
             <section class="description_prod">
-            Ce magnifique écran est, en plus, très lumineux.
+                <?php echo $Ecran["description"] ?>
             </section>
             <section>
-                <img class="photo_prod" src="https://www.cdiscount.com/pdt2/x/e/n/1/700x700/ls24a336nhuxen/rw/ecran-pc-samsung-s24a336nhu-24-fhd-dalle.jpg" alt="photo du produit">
+                <img class="photo_prod" src="<?php echo $Ecran["photo"] ?>" alt="photo du produit">
             </section>
             <section class="prix_prod">
-                Prix : 167 €
+                Prix : <?php echo $Ecran["price"] ?> €
             </section>
         </section>
 
--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -1,57 +1,117 @@
 <?php
-
-$name = string;
-$price = int;
-$photo = string;
-$description = string;
-
-$products = array(
-    $Souris = [
-        $name = 'Souris',
-        $price = '8',
-        $photo = 'https://www.grosbill.com/images_produits/fbab70f8-5842-4c66-9a8f-ac6d92543d64.jpg',
-        $description = 'Cette magnifique souris est, en plus, très ergonomique.',
-    ];
-    $Tour = [
-        $name = 'Tour',
-        "price" => "560",
-        "photo" => "https://www.ordi2-0.fr/wp-content/uploads/2021/02/Meilleur-ordinateur-tour-Sedatech-253x300.jpg",
-        "description" => "Cette magnifique tour est, en plus, très fonctionnelle.",
-    ];
-
-    $Clavier = [
-        "name" => "Clavier",
-        "price" => "270",
-        "photo" => "https://i0.wp.com/sesamepc.com/wp-content/uploads/2021/05/Clavier-US-HP-840-G1-G2-850-G1-G2-dessus.jpg?fit=1200%2C800&ssl=1",
-        "description" => "Ce magnifique clavier est, en plus, très pratique.",
-    ];
-
-    $Ecran = [
-        "name" => "Ecran",
-        "price" => "167",
-        "photo" => "https://www.cdiscount.com/pdt2/x/e/n/1/700x700/ls24a336nhuxen/rw/ecran-pc-samsung-s24a336nhu-24-fhd-dalle.jpg",
-        "description" => "Ce magnifique écran est, en plus, très lumineux.",
-    );
-
-?>
-
-
-
-Exemple du kit apprenant:
-<?php
 $products = [
-"iphone" => [
-"name" => "iPhone",
-"price" => 450,
-"weight" => 200,
-"discount" => 10,
-"picture_url" => "https://cdn-apple.com/iphone-12.jpg"
+"Souris" => [
+    "name" => "Souris",
+    "price" => "8",
+    "photo" => "https://www.grosbill.com/images_produits/fbab70f8-5842-4c66-9a8f-ac6d92543d64.jpg",
+    "description" => "Cette magnifique souris est, en plus, très ergonomique.",
 ],
-"ipad" => [
-"name" => "iPad",
-"price" => 450,
-"weight" => 400,
-"discount" => null,
-"picture_url" => "https://cdn-apple.com/ipad.jpg"
+
+"Tour" => [
+    "name" => "Tour",
+    "price" => "560",
+    "photo" => "https://www.ordi2-0.fr/wp-content/uploads/2021/02/Meilleur-ordinateur-tour-Sedatech-253x300.jpg",
+    "description" => "Cette magnifique tour est, en plus, très fonctionnelle.",
+],
+
+"Clavier" => [
+    "name" => "Clavier",
+    "price" => "270",
+    "photo" => "https://i0.wp.com/sesamepc.com/wp-content/uploads/2021/05/Clavier-US-HP-840-G1-G2-850-G1-G2-dessus.jpg?fit=1200%2C800&ssl=1",
+    "description" => "Ce magnifique clavier est, en plus, très pratique.",
+],
+
+"Ecran" => [
+    "name" => "Ecran",
+    "price" => "167",
+    "photo" => "https://www.cdiscount.com/pdt2/x/e/n/1/700x700/ls24a336nhuxen/rw/ecran-pc-samsung-s24a336nhu-24-fhd-dalle.jpg",
+    "description" => "Ce magnifique écran est, en plus, très lumineux.",
 ],
 ];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Catalogue produits</title>
+</head>
+
+<body>
+    <header>
+        <?php include 'header.php'; ?>
+    </header>
+
+    <div class="catalogue">
+
+        <section class="souris">
+            <section class="nom_prod">
+                <h2><?php echo $products["Souris"]["name"] ?></h2>
+            </section>  
+            <section class="description_prod">
+                <?php echo $products["Souris"]["description"] ?>
+            </section>
+            <section>
+                <img class="photo_prod" src="<?php echo $products["Souris"]["photo"] ?>" alt="photo du produit">
+            </section>
+            <section class="prix_prod">
+                Prix : <?php echo $products["Souris"]["price"] ?> €
+            </section>
+        </section>
+
+        <section class="tour">
+            <section class="nom_prod">
+                <h2><?php echo $products["Tour"]["name"] ?></h2>
+            </section>  
+            <section class="description_prod">
+                <?php echo $products["Tour"]["description"] ?>
+            </section>
+            <section>
+                <img class="photo_prod" src="<?php echo $products["Tour"]["photo"] ?>" alt="photo du produit">
+            </section>
+            <section class="prix_prod">
+                Prix : <?php echo $products["Tour"]["price"] ?> €
+            </section>
+        </section>
+
+        <section class="clavier">
+            <section class="nom_prod">
+                <h2><?php echo $products["Clavier"]["name"] ?></h2>
+            </section>  
+            <section class="description_prod">
+                <?php echo $products["Clavier"]["description"] ?>
+            </section>
+            <section>
+                <img class="photo_prod" src="<?php echo $products["Clavier"]["photo"] ?>" alt="photo du produit">
+            </section>
+            <section class="prix_prod">
+                Prix : <?php echo $products["Clavier"]["price"] ?> €
+            </section>
+        </section>
+
+        <section class="ecran">
+            <section class="nom_prod">
+                <h2><?php echo $products["Ecran"]["name"] ?></h2>
+            </section>  
+            <section class="description_prod">
+                <?php echo $products["Ecran"]["description"] ?>
+            </section>
+            <section>
+                <img class="photo_prod" src="<?php echo $products["Ecran"]["photo"] ?>" alt="photo du produit">
+            </section>
+            <section class="prix_prod">
+                Prix : <?php echo $products["Ecran"]["price"] ?> €
+            </section>
+        </section>
+
+    </div>
+
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
+</body>
+
+</html>
